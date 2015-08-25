135f9064a18a96141457099867ba66ff42852cf6
Vova Feldman <vova.feldman@gmail.com>
2015-08-25 14:55:13 +0000

    [refactor] [minor] Auto alignment of the code (no real code changes).

--- lib/rw-core-admin.php
+++ lib/rw-core-admin.php
@@ -1,26 +1,26 @@
 <?php
 	if ( ! defined( 'ABSPATH' ) ) exit;
 
-	if (!class_exists('RatingWidgetPlugin_Admin')) :
+	if (class_exists('RatingWidgetPlugin_Admin')) {
+		return;
+	}
 
-		class RatingWidgetPlugin_Admin {
-			public function __construct() {
+	class RatingWidgetPlugin_Admin {
+		public function __construct() {
 //        register_activation_hook(WP_RW__PLUGIN_FILE_FULL, array(&$this, 'Activated'));
 
-				add_action( 'admin_init', array( &$this, 'RedirectOnActivation' ) );
-			}
-
-			public function RedirectOnActivation() {
-				if ( get_option( 'rw_do_activation_redirect', false ) ) {
-					delete_option( 'rw_do_activation_redirect' );
-					wp_redirect( rw_get_admin_url() );
-					exit();
-				}
-			}
+			add_action( 'admin_init', array( &$this, 'RedirectOnActivation' ) );
 		}
 
-		function rw_admin() {
-			ratingwidget()->admin = new RatingWidgetPlugin_Admin();
+		public function RedirectOnActivation() {
+			if ( get_option( 'rw_do_activation_redirect', false ) ) {
+				delete_option( 'rw_do_activation_redirect' );
+				wp_redirect( rw_get_admin_url() );
+				exit();
+			}
 		}
+	}
 
-	endif;
+	function rw_admin() {
+		ratingwidget()->admin = new RatingWidgetPlugin_Admin();
+	}
