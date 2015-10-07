e9e740b8f653d9039e7843099f153a76348de634
Vova Feldman <vova.feldman@gmail.com>
2015-10-07 05:44:11 +0000

    [minor] Forgot to echo string.

--- view/preview.php
+++ view/preview.php
@@ -355,7 +355,7 @@
 	<input type="hidden" name="<?php echo rw_settings()->form_hidden_field_name; ?>" value="Y">
 	<input type="hidden" id="rw_options_hidden" name="rw_options" value=""/>
 
-	<input type="submit" name="Submit" class="button-primary" value="<?php esc_attr( __rw( 'save-changes' ) ) ?>"/>
+	<input type="submit" name="Submit" class="button-primary" value="<?php echo esc_attr( __rw( 'save-changes' ) ) ?>"/>
 	<?php if ( $rw_fs->is_not_paying() ) : ?>
 		<a href="<?php echo $rw_fs->get_upgrade_url() ?>"
 		   onclick="_gaq.push(['_trackEvent', 'upgrade', 'wordpress', 'gopro_button', 1, true]); _gaq.push(['_link', this.href]); return false;"
