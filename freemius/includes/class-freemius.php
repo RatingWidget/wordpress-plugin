0175299ca195ac3e125343d9e77c852d35b92724
Vova Feldman <vova@rating-widget.com>
2016-05-27 13:32:08 -0400

    [minor]

--- freemius/includes/class-freemius.php
+++ freemius/includes/class-freemius.php
@@ -2455,6 +2455,7 @@
 			     ! $this->has_features_enabled_license() &&
 			     ! $this->_has_premium_license()
 			) {
+					// IF wrapper is turned off because activation_timestamp is currently only stored for plugins (not addons).
 //                if (empty($this->_storage->activation_timestamp) ||
 //                    (WP_FS__SCRIPT_START_TIME - $this->_storage->activation_timestamp) > 30
 //                ) {
