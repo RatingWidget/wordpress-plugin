207fa5dbbb05e615f74b3a5817fe56bba141fb60
Leo Fajardo <leo@rating-widget.com>
2023-02-27 15:27:08 +0800

    [version] [rw] [fs] Updated version of Freemius with bug-fixes and new features (connectivity test removed).

--- freemius/start.php
+++ freemius/start.php
@@ -15,7 +15,7 @@
 	 *
 	 * @var string
 	 */
-	$this_sdk_version = '2.5.4';
+	$this_sdk_version = '2.5.4.1';
 
 	#region SDK Selection Logic --------------------------------------------------------------------
 
